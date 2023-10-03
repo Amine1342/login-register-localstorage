2ab86ee6da28f7c6426921f0969641a670459d14
Amine1342 <benabdelkader.amine1@gmail.com>
2023-10-03 17:43:31 +0100

    Page d'inscription terminée

--- JS/Register.js
+++ JS/Register.js
@@ -1,10 +1,65 @@
-function register() {
-var name = document.getElementById("name").value;
-var prenom = document.getElementById("prénom").value;
-var email = document.getElementById("email").value;
-var password = document.getElementById("password").value;
+function register(e) {
+    e.preventDefault();
 
-if (name == "" || prenom == "" || email == "" || password == "") {
+    var nom = document.getElementById("nom") ;
+    var prenom = document.getElementById("prénom") ;
+    var email = document.getElementById("email") ;
+    var password = document.getElementById("password") ;
+    var confirmpassword = document.getElementById("confirm") ;
+    var formValid = true;
+  
 
+    if (nom.value == "") {
+        nom.classList.remove("is-valid");
+        nom.classList.add("is-invalid");
+        formValid = false;
+    } else {
+        nom.classList.remove("is-invalid");
+        nom.classList.add("is-valid");
+        formValid = true;
+    }
+
+    if (prenom.value == "") {
+        prenom.classList.remove("is-valid");
+        prenom.classList.add("is-invalid");
+        formValid = false;
+    } else {
+        prenom.classList.remove("is-invalid");
+        prenom.classList.add("is-valid");
+        formValid = true;
+    }
+
+    if (email.value == "") {
+        email.classList.remove("is-valid");
+        email.classList.add("is-invalid");
+        formValid = false;
+    } else {
+        email.classList.remove("is-invalid");
+        email.classList.add("is-valid");
+        formValid = true;
+    }
+
+    if (password.value == "") {
+        password.classList.remove("is-valid");
+        password.classList.add("is-invalid");
+        formValid = false;
+    } else {
+        password.classList.remove("is-invalid");
+        password.classList.add("is-valid");
+        formValid = true;
+    }
+
+    if (confirmpassword.value == "" || confirmpassword.value != password.value) {
+        confirmpassword.classList.remove("is-valid");
+        confirmpassword.classList.add("is-invalid");
+        formValid = false;
+    } else {
+        confirmpassword.classList.remove("is-invalid");
+        confirmpassword.classList.add("is-valid");
+        formValid = true;
+    }
+   
 }
-}
+var btnregister = document.getElementById("submit")
+btnregister.addEventListener("click" , register)
+console.log('name ', nom.value);
