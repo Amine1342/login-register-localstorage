195829b7faf3598203745389f87731103367be98
Amine1342 <benabdelkader.amine1@gmail.com>
2023-10-03 16:53:41 +0100

    first commit

---
+++ JS/Register.js
@@ -0,0 +1,10 @@
+function register() {
+var name = document.getElementById("name").value;
+var prenom = document.getElementById("prénom").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+
+if (name == "" || prenom == "" || email == "" || password == "") {
+
+}
+}
